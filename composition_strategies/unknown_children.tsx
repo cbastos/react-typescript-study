143f2a860416611e891a14bd52d131a5f949f986
cbastos <carlosbastospc@gmail.com>
2021-08-30 18:59:12 +0200

    1 file modified

---
+++ composition_strategies/unknown_children.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+const App: React.FC = ({ children }) => {
+  return <div>{children}</div>;
+};
+
+const LeftBar: React.FC = () => {
+  return (
+    <ul style={{ border: '1px solid black', marginTop: 2, width: '20%' }}>
+      <li>Page 1</li>
+      <li>Page 2</li>
+      <li>Page 3</li>
+    </ul>
+  );
+};
+
+const TopBar: React.FC = () => {
+  return (
+    <div style={{ width: '100%', border: '1px solid black' }}>Top bar</div>
+  );
+};
+
+render(
+  <App>
+    <TopBar />
+    <LeftBar />
+  </App>,
+  document.getElementById('root')
+);
